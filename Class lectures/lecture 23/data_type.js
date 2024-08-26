c1c05559b0cbdf046f9f611bcc50f2aca05a81a5
navjot0079 <157043540+navjot0079@users.noreply.github.com>
2024-08-26 21:29:38 +0530

    uploaded

---
+++ Class lectures/lecture 23/data_type.js
@@ -0,0 +1,30 @@
+// 1.Number
+let a=10;
+let b=-3;
+let c=2.3;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+// 2.String
+
+let name = "Navjot";
+console.log(typeof(name));
+
+// 3.Boolean
+
+let x= true;
+console.log(typeof(x));
+
+// 4.undefined
+
+let y;
+console.log(typeof(y));
+
+//5. null
+let j = null;
+console.log(typeof(j));
+
+let i=5;
+i+=2;
+console.log(i);
---
+++ Class lectures/lecture 23/variable.js
@@ -0,0 +1,18 @@
+//var: can be declared and updated
+//var a=10;
+//console.log(a);
+
+//var a=20;
+//console.log(a);
+
+// let: can not be declared but can be updated
+let a=10;
+
+a=20;
+console.log(a);
+
+//const:cannot be declared and updated
+//const a=10;
+
+//a=20; error
+//console.log(a);
